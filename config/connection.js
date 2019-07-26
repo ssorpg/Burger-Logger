f97e159a43e3745cc1f1233e5ab8e4f1b1749547
Jonathan Baker <ssorpg@gmail.com>
2019-07-25 22:42:30 -0700

    i got out of bed to make this change

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const keys = require('./keys');
 // MYSQL PARAMS
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
+    user: keys.mySQL.username,
     password: keys.mySQL.password,
     database: 'burgers_db'
 });
