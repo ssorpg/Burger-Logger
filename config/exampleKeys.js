5d748c85abb27c7b180f7b45bc9ce13b64f73cea
Jonathan Baker <ssorpg@gmail.com>
2019-07-27 12:58:50 -0700

    made readme more verbose and added examplekeys.js so i dont accidentally upload my keys anymore

---
+++ config/exampleKeys.js
@@ -0,0 +1,14 @@
+// MYSQL KEY
+const mySQL = {
+    host: 'localhost',
+    username: 'yourusername',
+    password: 'yourpassword',
+    database: 'burgers_db'
+};
+
+
+
+// EXPORTS
+module.exports = {
+    mySQL: mySQL
+};
